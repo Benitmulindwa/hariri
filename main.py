56007193c3c5e3b61c080297790ae6a40310a122
Benitmulindwa <mulindwabenit@gmail.com>
2023-11-09 14:16:45 +0100

    Implement save, save as and open file functionalities

--- main.py
+++ main.py
@@ -17,10 +17,15 @@ class CodeEditor(ft.UserControl):
     def __init__(self, page):
         super().__init__()
         self.page = page
+        self.title_suffix = " -Hariri"
+        self.page.title = "New File" + self.title_suffix
+
         self.page.on_keyboard_event = self.on_keyboard
 
     def build(self):
-        # App bar
+        ## APPBAR ##
+
+        # File popupmenu
         self.file_menu = ft.PopupMenuButton(
             content=Text("File", size=15),
             items=[
@@ -58,7 +63,7 @@ class CodeEditor(ft.UserControl):
                     ),
                 ),
                 ft.PopupMenuItem(
-                    on_click=self.save_as_clicked,
+                    on_click=self.save_as,
                     content=Row(
                         alignment=MainAxisAlignment.SPACE_BETWEEN,
                         controls=[
@@ -81,6 +86,8 @@ class CodeEditor(ft.UserControl):
             ],
         )
 
+        # Icons Buttons
+
         self.page.overlay.append(
             Container(
                 padding=5,
@@ -93,8 +100,8 @@ class CodeEditor(ft.UserControl):
         return Column([Divider(opacity=0), self.main_ft])
 
     def new_clicked(self, e):
-        self.value = ""
-        self.update()
+        self.main_ft.value = ""
+        self.main_ft.update()
 
     # Open a file
     def open_clicked(self, e):
@@ -103,20 +110,67 @@ class CodeEditor(ft.UserControl):
         self.page.update()
 
         file_picker.pick_files(
-            allow_multiple=False, allowed_extensions="py", dialog_title="Open File"
+            allow_multiple=False, allowed_extensions=["py"], dialog_title="Open File"
         )
 
     def open_file_result(self, e: FilePickerResultEvent):
-        print(e.files)
+        file_path = e.files[0].path
+        self.file_name = e.files[0].name
+        self.page.title = self.file_name + self.title_suffix
+
+        with open(file_path, "r") as file:
+            self.main_ft.value = file.read()
+            self.main_ft.update()
+            self._snackbar(f"File {file_path} Opened", Icon(icons.CHECK, color="black"))
+        self.page.update()
 
     def save_clicked(self, e):
-        print("save clicked")
+        if self.current_file_path == "":
+            self.save_as(e)
+        with open(self.current_file_path, "r") as file:
+            self.is_different = self.main_ft.value != file.read()
+            if self.is_different:
+                with open(self.current_file_path, "w") as new_script:
+                    new_script.write(self.main_ft.value)
+                    self.is_code_different = False
+                    self._snackbar("Saved", Icon(icons.THUMB_UP_OFF_ALT))
+                    self.page.update()
+            else:
+                pass
 
-    def save_as_clicked(self, e):
-        print("Save_as clicked")
+    def save_as(self, e):
+        file_picker = FilePicker(on_result=self.save_as_result)
+        self.page.overlay.append(file_picker)
+        self.page.update()
+
+        file_picker.save_file(
+            file_name="New File", allowed_extensions=["py"], dialog_title="Save As"
+        )
+
+    def save_as_result(self, e: ft.FilePickerResultEvent):
+        with open(e.path, "w") as file:
+            file.write(self.main_ft.value)
+            self.current_file_path = e.path
+
+        file_name = e.path.split("\\")
+        self.page.title = file_name[-1] + self.title_suffix
+        self._snackbar(f"Saved As {file_name[-1]}", Icon(icons.CHECK, color="green"))
+        self.page.update()
 
     def exit(self, e):
         print("exit clicked")
+
+    # snackbar
+
+    def _snackbar(self, text: str, icon: Icon):
+        self.page.snack_bar = SnackBar(
+            bgcolor="#427d9d",
+            open=True,
+            content=Row(
+                alignment=MainAxisAlignment.CENTER,
+                controls=[Text(text, color="black"), icon],
+            ),
+        )
 
     # formatting the code
 
@@ -141,7 +195,7 @@ class CodeEditor(ft.UserControl):
             print(self.value.splitlines()[-1])
 
     def run(self, e):
-        code = self.value
+        code = self.main_ft.value
         exec(code)
 
 
